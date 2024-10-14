f9d57e502edbe8c90e270418355c3f0a78676076
emanuelsmcastro-dev <emanuelsmcastro3@gmail.com>
2024-10-14 12:13:59 -0300

    Update User.java

--- oauth-ms/src/main/java/com/oauth/server/entities/User.java
+++ oauth-ms/src/main/java/com/oauth/server/entities/User.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(unique = true)
 	private String username;
 	private String password;
 	@OneToOne
