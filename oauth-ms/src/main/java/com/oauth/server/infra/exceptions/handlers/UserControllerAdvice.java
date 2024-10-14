bdc0baa708fb4b74870db0af17d3543ddb16b9a7
emanuelsmcastro-dev <emanuelsmcastro3@gmail.com>
2024-10-14 14:17:12 -0300

    Create UserControllerAdvice.java

---
+++ oauth-ms/src/main/java/com/oauth/server/infra/exceptions/handlers/UserControllerAdvice.java
@@ -0,0 +1,25 @@
+package com.oauth.server.infra.exceptions.handlers;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.oauth.server.dtos.v1.exceptions.ExceptionResponse;
+import com.oauth.server.infra.exceptions.RegistrationException;
+
+@ControllerAdvice
+public class UserControllerAdvice {
+
+	@ExceptionHandler(RegistrationException.class)
+	public ResponseEntity<ExceptionResponse> registrationExceptionHandler(RegistrationException ex,
+			WebRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(), status.value(), ex.getMessage(),
+				request.getDescription(false));
+		return ResponseEntity.status(status.value()).body(response);
+	}
+}
