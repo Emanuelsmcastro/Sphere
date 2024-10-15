9dbba6a830d5c50c5415465cf359dd083050b73f
emanuelsmcastro-dev <emanuelsmcastro3@gmail.com>
2024-10-15 17:03:54 -0300

    create: contact component

---
+++ frontend/src/components/contacts.js
@@ -0,0 +1,320 @@
+import styles from '../static/css/contacts.module.css';
+
+function Contacts(){
+    return (
+        <div className={styles.contactContainer}>
+            <h3 className={styles.contactTitle}>Contacts</h3>
+            <hr />
+            <ul className={styles.contactList}>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact"
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact" 
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact"
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact" 
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact"
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact" 
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact" 
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact"
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact" 
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact"
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact" 
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact"
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact" 
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact"
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact" 
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact" 
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact"
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div style={{ paddingLeft: '8px', paddingRight: '8px' }}>
+                        <a href="" role="link" tabIndex="0" style={{ display: 'flex', alignItems: 'center' }}>
+                            <div style={{ marginRight: '8px' }}>
+                                <img 
+                                    src="https://as2.ftcdn.net/v2/jpg/01/04/70/49/1000_F_104704911_qDKDQEttQEsKpf3dioPxCkKCx30PaPuH.jpg"
+                                    alt="Contact" 
+                                    style={{ height: '36px', width: '36px', borderRadius: '50%' }} 
+                                />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <span style={{ fontWeight: 'bold' }}>Someone</span>
+                                <span style={{ color: 'green' }}>Online</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Contacts;
