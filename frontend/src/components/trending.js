66fd07ee20cb6ffdef5e84052edcd8d3e7511838
emanuelsmcastro-dev <emanuelsmcastro3@gmail.com>
2024-10-15 17:04:34 -0300

    create: trending component

---
+++ frontend/src/components/trending.js
@@ -0,0 +1,50 @@
+import styles from '../static/css/trending.module.css';
+
+function Trending(){
+    return (
+        <div className={styles.trendingContainer}>
+            <h3 className={styles.trendingTitle}>Trending Topics</h3>
+            <hr />
+            <ul className={styles.trendingList}>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+                <li><a href="">#Something</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Trending;
