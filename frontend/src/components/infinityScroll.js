0d4f8e9b4f8a6cbce58dffb4786f8f9e26fb52c4
emanuelsmcastro-dev <emanuelsmcastro3@gmail.com>
2024-10-29 12:16:02 -0300

    update: chat with history and infinity scroll

--- frontend/src/components/infinityScroll.js
+++ frontend/src/components/infinityScroll.js
@@ -1,19 +1,22 @@
 import { useEffect } from "react";
 
-function InfiniteScroll({ containerRef, loadMore, hasMore, children}) {
+function InfiniteScroll({ containerRef, loadMore, hasMore, reversed, children}) {
 
     useEffect(() => {
         const container = containerRef.current;
 
         const handleScroll = () => {
-            if (container.scrollHeight - container.scrollTop === container.clientHeight && hasMore) {
+            
+            if (container.scrollHeight - container.scrollTop === container.clientHeight && hasMore && !reversed) {
+                loadMore();
+            } else if(container.scrollTop === 0 && hasMore){
                 loadMore();
             }
         };
 
         container.addEventListener("scroll", handleScroll);
         return () => container.removeEventListener("scroll", handleScroll);
-    }, [loadMore, hasMore, containerRef]);
+    }, [loadMore, hasMore, containerRef, reversed]);
 
     return (
         <>
